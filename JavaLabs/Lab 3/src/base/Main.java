d2a5c998a63535758d74ff264b81afb48d5e8425
jumin7 <89848112+jumin7@users.noreply.github.com>
2022-02-02 19:12:48 -0500

    First Part
    First three questions solved.

--- JavaLabs/Lab 3/src/base/Main.java
+++ JavaLabs/Lab 3/src/base/Main.java
@@ -13,6 +13,15 @@ public class Main {
      *     - Else If
      */
     public static void main(String[] args) {
+        int x = 7;
+        int y = 9;
+        if (x < y) {
+            System.out.println ("x is smaller than y");
+        } else if ( y < x) {
+            System.out.println ("y is smaller than x");
+        } else {
+            System.out.println ("there was an error");
+        }
         // Make an if statement that triggers a print or println statement
         // Make an if, else statement where the else statement triggers a print or println statement
         // Make an if, else if, else statement where the else if statement triggers a print or println statement
