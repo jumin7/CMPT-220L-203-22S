303485b7eb9e3df8df9333f74214719b01c23da4
jumin7 <89848112+jumin7@users.noreply.github.com>
2022-02-03 18:23:18 -0500

    Final Submission
    Final draft of the Lab. All of the questions solved successfully.

--- JavaLabs/Lab 3/src/base/Main.java
+++ JavaLabs/Lab 3/src/base/Main.java
@@ -1,6 +1,5 @@
 package base;
 
-import java.util.Scanner;
 
 public class Main {
     /**
@@ -16,27 +15,60 @@ public class Main {
      */
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        int num1, num2;
-        System.out.println ("Enter a random integer");
-        num1 = scan.nextInt();
-        System.out.println ("Enter another random integer");
-        num2 = scan.nextInt();
 
-
-        
-        if (num1< num2) {
-            System.out.println ("The first number is smaller than second");
-        } else if ( num2 < num1) {
-            System.out.println ("The second number is smaller than first");
-        } else {
-            System.out.println ("they are equal");
-        }
         // Make an if statement that triggers a print or println statement
+        int x = 10;
+
+        if (x < 20) {
+            System.out.println(" X is less than 20.");
+        }
+
         // Make an if, else statement where the else statement triggers a print or println statement
+        int num = 2;
+
+        if (num > x){
+            System.out.println ("2 is greater than x");
+        } else {
+            System.out.println ("num 1 is smalleer than x .");
+        }
         // Make an if, else if, else statement where the else if statement triggers a print or println statement
+
+        int a = 2;
+
+        if (a != 2) {
+            System.out.println("Not equal");
+        } else if (a == 2){
+            System.out.println ("a is equal to 2.");
+        } else {
+            System.out.println ("Something is wrong");
+        }
+
         // Make 2 variables and use them in an if else conditional print from one of the sections
+        int age = 18;
+        int height = 150;
+        
+
+        if (age >= 18){
+            System.out.println (" You are eligible.");
+        } else {
+            System.out.println (" You are not eligible");
+        }
+
         // Make an if statement using 2 variables and an AND(&&) statement that triggers a print or println statement
+        if ((age >= 18) && (height >= 150)){
+            System.out.println ("You are eligible to compete.");
+        }else {
+            System.out.println ("Something went wrong");
+        }
+
         // Make an if statement using 2 variables and an OR(||) statement that triggers a print or println statement
+        int age2 =16;
+        int height2 =152;
+
+        if ((age2 < 18) || (height2 < 150)){
+            System.out.println ("You are not eligible to compete in the game.");
+        }else {
+            System.out.println ("Something is wrong");
+        }
     }
 }
