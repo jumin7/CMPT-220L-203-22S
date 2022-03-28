7a4019c588f949f9320422fde423d5d26b797326
jumin7 <89848112+jumin7@users.noreply.github.com>
2022-03-28 14:44:35 -0400

    Lab 7 complete

---
+++ JavaLabs/Lab7/src/ListItem.java
@@ -0,0 +1,12 @@
+public class ListItem {
+    int value;
+    ListItem listItem;
+
+
+    ListItem(int newValue, ListItem newListItem) {
+        value = newValue;
+        listItem = newListItem;
+
+    }
+
+}
---
+++ JavaLabs/Lab7/src/main.java
@@ -0,0 +1,15 @@
+public class main {
+    public main() {
+    }
+
+    public static void main(String[] args) {
+
+        ListItem a = new ListItem(0, null);
+        ListItem b = new ListItem(1, a);
+        ListItem c = new ListItem(2, b);
+        ListItem d = new ListItem(3, c);
+        ListItem e = new ListItem(4, d);
+        
+
+    }
+}
